9a1dea468a4a666cd3912238ccd9c4b78535640f
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2023-07-20 18:01:14 +0530

    Updated

--- script.js
+++ script.js
@@ -11,9 +11,6 @@ let readBox = document.getElementById('check-read')
 
 
 
-let readButton = document.createElement('button')
-let removeButton = document.createElement('button')
-
 
 
 // form
@@ -70,46 +67,73 @@ function displayMyLibrary(){
 
     // creates new div, h2 and p
    
+    for (let i=0; i<tempLibrary.length;i++){
+        const currentBook = tempLibrary[i]
+        let newDiv = document.createElement('div')
+        let newTitle = document.createElement('h2')
+        let newAuthor = document.createElement('h3')
+        let newPage = document.createElement('p')
+        let horizontalLine =document.createElement('hr')
+        let readButton = document.createElement('button')
+        let removeButton = document.createElement('button')
+        let readLabel = document.createElement('div')
 
-    let newDiv = document.createElement('div')
-    let newTitle = document.createElement('h2')
-    let newAuthor = document.createElement('h3')
-    let newPage = document.createElement('p')
-    let horizontalLine =document.createElement('hr')
-    let readLabel = document.createElement('div')
 
 
 
 
+        
 
 
-    // adds class lists to div, h2, p
-    newDiv.classList.add('book-border')
+        // adds class lists to div, h2, p
+        newDiv.classList.add('book-border')
+        readButton.classList.add('button-read')
   
+    
+
+        
+        // displays user entry inside the divs
+        newTitle.textContent = currentBook.title
+        newAuthor.textContent = `Author - ${currentBook.author}`
+        newPage.textContent = `Number Of Pages - ${currentBook.page}`
+
+        readButton.textContent = 'Read'
+        //removeButton.textContent = 'Remove This Book'
+        tempLibrary = []
+    
+
+
+
+        // appends new div, h2, p to content div
+
+        content.appendChild(newDiv)
+        newDiv.appendChild(newTitle)
+        newDiv.appendChild(newAuthor)
+        newDiv.appendChild(newPage)
+        newDiv.appendChild(readButton)
+        //newDiv.appendChild(removeButton)
+        newDiv.appendChild(readLabel)
+        newTitle.appendChild(horizontalLine)
+
+        // checkbox data
+
+        readButton.addEventListener('click' , () => {
+            if (readButton.textContent === 'Read'){
+                readButton.textContent = 'Not Read'
+                readLabel.classList.add('read')
+                readLabel.classList.remove('not-read')
+            }
+            else{
+                readButton.textContent = 'Read'
+                readLabel.classList.add('not-read')
+                readLabel.classList.remove('read')
+
+            }
+    
+        })
 
     
-    // displays user entry inside the divs
-    newTitle.textContent = tempLibrary.map(book => book.title)
-    newAuthor.textContent = `Author - ${tempLibrary.map(book => book.author)}`
-    newPage.textContent = `Number Of Pages - ${tempLibrary.map(book => book.page)}`
-
-    removeButton.textContent = 'Remove'
-    tempLibrary = []
-   
-
-
-
-    // appends new div, h2, p to content div
-
-    content.appendChild(newDiv)
-    newDiv.appendChild(newTitle)
-    newDiv.appendChild(newAuthor)
-    newDiv.appendChild(newPage)
-    newDiv.appendChild(readLabel)
-    newTitle.appendChild(horizontalLine)
-
-    // checkbox data
-
+    }
 }
 
 
